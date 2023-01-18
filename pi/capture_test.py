3bffaf2956e855d9f880865f9b31f3bbb4428b72
Phil D'Amore <ptdamore@gmail.com>
2023-01-17 21:03:59 -0500

    update

--- pi/capture_test.py
+++ pi/capture_test.py
@@ -11,13 +11,23 @@ import colorsys
 import tensorflow as tf
 from typing import NamedTuple
 import os
+import uuid
 
 # initialize the camera and grab a reference to the raw camera capture
 w = 400
 h = 400
 camera = PiCamera(sensor_mode=4, framerate=30, resolution=(w, h))
 
-reverse_label_map = {0: 'arresto-momentum', 1: 'descendo', 2: 'incendio', 3: 'mimblewimble', 4: 'locomotor', 5: 'tarantallegra', 6: 'crap'}
+reverse_label_map = {
+    0: "arresto-momentum",
+    1: "descendo",
+    2: "incendio",
+    3: "mimblewimble",
+    4: "locomotor",
+    5: "tarantallegra",
+    6: "gonadium",
+}
+
 
 def create_pass1_detector():
     params = cv2.SimpleBlobDetector_Params()
@@ -82,11 +92,13 @@ loser_bin = collections.Counter()
 loser_timestamps = {}
 loser_bin_evictions_counter = 0
 loser_bin_stats_counter = 0
-counter = max([2000] + [int(f.split('.')[0]) for f in os.listdir('./captures')]) + 1
-print(f"Starting with file index: {counter}")
 
+clear_reason = ""
+dwell_timer = 0
 
 def render(raw_data_grouped):
+    global clear_reason
+    global dwell_timer
     ##### PASTE
     min_frame = raw_data_grouped[0][0][2]
     filtered_frames = []
@@ -102,34 +114,32 @@ def render(raw_data_grouped):
             return
         filtered_frames.append(f)
 
+    def is_loser(pt):
+        ret = (int(pt[0][0] / bin_size), int(pt[0][1] / bin_size)) in loser_bin
+        if ret:
+            global loser_bin_evictions_counter
+            loser_bin_evictions_counter += 1
+        return ret
+
+    def add_loser(pt):
+        loser_bin[(int(pt[0][0] / bin_size), int(pt[0][1] / bin_size))] += 1
+        loser_timestamps[(int(pt[0][0] / bin_size), int(pt[0][1] / bin_size))] = time()
+
+    def trim_losers():
+        to_remove = []
+        for loser, timestamp in loser_timestamps.items():
+            if (time() - timestamp) > 30:
+                to_remove.append(loser)
+        for winner in to_remove:
+            del loser_bin[winner]
+            del loser_timestamps[winner]
+
     for frame in raw_data_grouped:
         formatted_frame = [[(pt[0], pt[1]), pt[2] - min_frame] for pt in frame]
         frame_q.append(formatted_frame)
         if len(frame_q) < 3:
             # Don't do anything until we have enough points to vote
             continue
-
-        def is_loser(pt):
-            ret = (int(pt[0][0] / bin_size), int(pt[0][1] / bin_size)) in loser_bin
-            if ret:
-                global loser_bin_evictions_counter
-                loser_bin_evictions_counter += 1
-            return ret
-
-        def add_loser(pt):
-            loser_bin[(int(pt[0][0] / bin_size), int(pt[0][1] / bin_size))] += 1
-            loser_timestamps[
-                (int(pt[0][0] / bin_size), int(pt[0][1] / bin_size))
-            ] = time()
-
-        def trim_losers():
-            to_remove = []
-            for loser, timestamp in loser_timestamps.items():
-                if (time() - timestamp) > 30:
-                    to_remove.append(loser)
-            for winner in to_remove:
-                del loser_bin[winner]
-                del loser_timestamps[winner]
 
         if len(frame_q[0]) == 1:
             # If the frame we are processing only has one hit
@@ -201,164 +211,208 @@ def render(raw_data_grouped):
     # print(f"{time()-start_ts}")
     ##### PASTE END
 
-    if len(filtered_frames) >= 3:
-        # Test for stillness
-        v0 = np.array(filtered_frames[-2][0]) - np.array(filtered_frames[-3][0])
-        v1 = np.array(filtered_frames[-1][0]) - np.array(filtered_frames[-2][0])
-        total_dist = np.linalg.norm(v0) + np.linalg.norm(v1)
-        if total_dist > 4:
+    global detect_queue_pass1
+    if len(filtered_frames) >= 10:
+        # Test for motion
+        strides = np.array(
+            [i for i in zip(filtered_frames[-10:], filtered_frames[-9:])]
+        )
+        vectors = []
+        for stride in strides:
+            vectors.append(np.array(stride[1][0]) - np.array(stride[0][0]))
+        resultant_vector = sum(vectors)
+        total_dist = np.linalg.norm(resultant_vector)
+        if total_dist > 10:
+            dwell_timer = 0
             return
+        
 
         xmax = max([frame[0][0] for frame in filtered_frames])
         ymax = max([frame[0][1] for frame in filtered_frames])
         xmin = min([frame[0][0] for frame in filtered_frames])
         ymin = min([frame[0][1] for frame in filtered_frames])
 
-        dim = max([xmax - xmin, ymax - ymin])
+        dominant_dimension = np.max([xmax - xmin, ymax - ymin])
+        dominant_dimension += 40 - (dominant_dimension % 40)
 
-        # print(f"{ymax},{ymin},{xmax},{xmin},{dim}")
+        # Frames smaller than this are unlikely to have anything
+        # interesting so don't process them
+        if dominant_dimension <= 80:
+            if dwell_timer < 2:
+                dwell_timer += 1
+                if dwell_timer == 2:
+                    # Only allow this once and then not again
+                    # until we detect more movement
+                    clear_reason = "dwell"
+                    detect_queue_pass1.clear()
+            return
 
-        processed_output_image = np.zeros((dim, dim), dtype=np.uint8)
+        xpad = int((dominant_dimension - (xmax - xmin)) / 2)
+        ypad = int((dominant_dimension - (ymax - ymin)) / 2)
+
+        processed_output_image = np.zeros(
+            (dominant_dimension, dominant_dimension), dtype=np.uint8
+        )
 
         prev_x = None
         prev_y = None
         for frame in filtered_frames:
             if prev_x is None or prev_y is None:
-                prev_x = frame[0][0] - xmin
-                prev_y = frame[0][1] - ymin
+                prev_x = frame[0][0] - xmin + xpad
+                prev_y = frame[0][1] - ymin + ypad
                 continue
             start = (prev_x, prev_y)
-            end = (frame[0][0] - xmin, frame[0][1] - ymin)
+            end = (frame[0][0] - xmin + xpad, frame[0][1] - ymin + ypad)
             cv2.line(
-                processed_output_image, start, end, color=(255, 255, 255), thickness=3
+                processed_output_image, start, end, color=(255, 255, 255), thickness=2
             )
             prev_x = end[0]
             prev_y = end[1]
 
-            # cv2.putText(
-            #     processed_output_image,
-            #     f"{len(filtered_frames)}",
-            #     (50, 50),
-            #     cv2.FONT_HERSHEY_SIMPLEX,
-            #     1,
-            #     (255, 255, 255),
-            #     2,
-            # )
         try:
-            disp_process_image = cv2.resize(processed_output_image, (40, 40))
+            disp_process_image = cv2.resize(
+                processed_output_image,
+                (40, 40),
+                # interpolation = cv2.INTER_CUBIC
+                interpolation=cv2.INTER_AREA,
+            )
         except Exception as e:
-            # print(f"resize fail, skipping: {e}")
+            print(f"RESIZE FAIL")
             return
-        cv2.imshow("Processed", disp_process_image)
-        # global counter
-        # print(f"write captures/{counter}.png")
-        # cv2.imwrite(filename=f"captures/{counter}.png", img=disp_process_image)
-        # counter += 1
+        cv2.imshow("Processed", processed_output_image)
+        disp_process_image[disp_process_image > 0] = 255
         formatted_img_array = np.expand_dims(
             disp_process_image.reshape(40, 40, 1), axis=0
         )
-        # # for i in range(40):
-        # #     row = formatted_img_array[0][i]
-        # #     print([i[0] for i in row])
-        model_info.interpreter.set_tensor(model_info.input[0]['index'], formatted_img_array)
+        model_info.interpreter.set_tensor(
+            model_info.input[0]["index"], formatted_img_array
+        )
         model_info.interpreter.invoke()
-        output_data = model_info.interpreter.get_tensor(model_info.output[0]['index'])[0]
+        output_data = model_info.interpreter.get_tensor(model_info.output[0]["index"])[
+            0
+        ]
         pred_max = np.max(output_data)
         pred_argmax = np.argmax(output_data)
         # if pred_max > 200 and reverse_label_map[pred_argmax] != 'crap':
         if True:
             print(f"pred: {pred_argmax} ({reverse_label_map[pred_argmax]}): {pred_max}")
+
+        label_image = np.zeros((200, 400), dtype=np.uint8)
+        cv2.putText(
+            label_image,
+            f"{reverse_label_map[pred_argmax]} ({pred_max/255:.02f})",
+            (10, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            2,
+        )
+        cv2.imshow("Label", label_image)
+
+        outfiledir = os.path.join("captures", reverse_label_map[pred_argmax])
+        outfilename = os.path.join(outfiledir, f"{uuid.uuid4()}.png")
+        os.makedirs(outfiledir, exist_ok=True)
+        # print(f"write {outfilename} ({np.count_nonzero(disp_process_image[(disp_process_image!=0) & (disp_process_image!=255)])})")
+        cv2.imwrite(filename=outfilename, img=disp_process_image)
         # print(f"Execution time: {time() - start_ts}")
 
-    # raw_output_image = np.zeros((400, 450, 3), dtype=np.uint8)
-    # prev_x = None
-    # prev_y = None
-    # for frame in [(p[0], p[1]) for frame in raw_data_grouped for p in frame]:
-    #     if prev_x is None or prev_y is None:
-    #         prev_x = frame[0]
-    #         prev_y = frame[1]
-    #         continue
-    #     start = (prev_x, prev_y)
-    #     end = (frame[0], frame[1])
-    #     cv2.line(raw_output_image, start, end, color=(255, 255, 255), thickness=3)
-    #     prev_x = end[0]
-    #     prev_y = end[1]
+        # raw_output_image = np.zeros((400, 450, 3), dtype=np.uint8)
+        # # prev_x = None
+        # # prev_y = None
+        # # for frame in [(p[0], p[1]) for frame in raw_data_grouped for p in frame]:
+        # #     if prev_x is None or prev_y is None:
+        # #         prev_x = frame[0]
+        # #         prev_y = frame[1]
+        # #         continue
+        # #     start = (prev_x, prev_y)
+        # #     end = (frame[0], frame[1])
+        # #     cv2.line(raw_output_image, start, end, color=(255, 255, 255), thickness=3)
+        # #     prev_x = end[0]
+        # #     prev_y = end[1]
 
-    # for i in range(255):
-    #     color = np.array(colorsys.hsv_to_rgb(i / 255, 1, 1)) * 255
-    #     color = color.astype(np.uint8).tolist()
-    #     cv2.rectangle(
-    #         raw_output_image,
-    #         (445, 399 - ((2 * i) + 1)),
-    #         (449, 399 - ((2 * i))),
-    #         color=color,
-    #         thickness=-1,
-    #     )
-    #     cv2.putText(
-    #         raw_output_image,
-    #         "255",
-    #         (400, 20),
-    #         cv2.FONT_HERSHEY_SIMPLEX,
-    #         .5,
-    #         (255, 255, 255),
-    #         1,
-    #     )
-    #     cv2.putText(
-    #         raw_output_image,
-    #         "1",
-    #         (425, 390),
-    #         cv2.FONT_HERSHEY_SIMPLEX,
-    #         .5,
-    #         (255, 255, 255),
-    #         1,
-    #     )
+        # for i in range(255):
+        #     color = np.array(colorsys.hsv_to_rgb(i / 255, 1, 1)) * 255
+        #     color = color.astype(np.uint8).tolist()
+        #     cv2.rectangle(
+        #         raw_output_image,
+        #         (445, 399 - ((2 * i) + 1)),
+        #         (449, 399 - ((2 * i))),
+        #         color=color,
+        #         thickness=-1,
+        #     )
+        #     cv2.putText(
+        #         raw_output_image,
+        #         "255",
+        #         (400, 20),
+        #         cv2.FONT_HERSHEY_SIMPLEX,
+        #         0.5,
+        #         (255, 255, 255),
+        #         1,
+        #     )
+        #     cv2.putText(
+        #         raw_output_image,
+        #         "1",
+        #         (425, 390),
+        #         cv2.FONT_HERSHEY_SIMPLEX,
+        #         0.5,
+        #         (255, 255, 255),
+        #         1,
+        #     )
 
-    # for frame in [(p[0], p[1], v) for p, v in loser_bin.items()]:
-    #     if frame[2] < 255:
-    #         color = np.array(colorsys.hsv_to_rgb(frame[2] / 255, 1, 1)) * 255
-    #     else:
-    #         color = np.array(colorsys.hsv_to_rgb(1, 1, 1)) * 255
-    #     color = color.astype(np.uint8).tolist()
-    #     cv2.rectangle(
-    #         raw_output_image,
-    #         (frame[0] * bin_size, frame[1] * bin_size),
-    #         (int((frame[0] * bin_size) + bin_size), int((frame[1] * bin_size) + bin_size)),
-    #         color=color,
-    #         thickness=-1,
-    #     )
+        # for frame in [(p[0], p[1], v) for p, v in loser_bin.items()]:
+        #     if frame[2] < 255:
+        #         color = np.array(colorsys.hsv_to_rgb(frame[2] / 255, 1, 1)) * 255
+        #     else:
+        #         color = np.array(colorsys.hsv_to_rgb(1, 1, 1)) * 255
+        #     color = color.astype(np.uint8).tolist()
+        #     cv2.rectangle(
+        #         raw_output_image,
+        #         (frame[0] * bin_size, frame[1] * bin_size),
+        #         (
+        #             int((frame[0] * bin_size) + bin_size),
+        #             int((frame[1] * bin_size) + bin_size),
+        #         ),
+        #         color=color,
+        #         thickness=-1,
+        #     )
 
-    # cv2.imshow("Raw", raw_output_image)
+        # cv2.imshow("Raw", raw_output_image)
+
+        clear_reason = "inference"
+        detect_queue_pass1.clear()
 
 
 executor = ThreadPoolExecutor(max_workers=1)
 
-# capture frames from the camera
 def is_global_loser(kp):
     bin_size = 20
     ret = (int(kp.pt[0] / bin_size), int(kp.pt[1] / bin_size)) in loser_bin
     return ret
 
+# capture frames from the camera
 miss_counter = 0
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image_pass1 = cv2.cvtColor(cv2.flip(frame.array, 1), cv2.COLOR_BGR2GRAY)
-    raw_points_image = np.zeros((400, 400), dtype=np.uint8)
+    raw_points_image = np.zeros((400, 400, 3), dtype=np.uint8)
     keypoints = detector_pass1.detect(image_pass1)
     try:
+        formatted_new_point = []
         if keypoints:
-            formatted_new_point = [(int(kp.pt[0]), int(kp.pt[1]), frame_count) for kp in keypoints if not is_global_loser(kp)]
+            formatted_new_point = [
+                (int(kp.pt[0]), int(kp.pt[1]), frame_count)
+                for kp in keypoints
+                if not is_global_loser(kp)
+            ]
             if formatted_new_point:
-                detect_queue_pass1.append(
-                    formatted_new_point
-                )
-                if miss_counter:
-                    miss_counter -= 1
-            else:
-                if miss_counter < 5:
-                    miss_counter += 1
-                if miss_counter == 5:
-                    detect_queue_pass1.popleft()
-        else:
-            detect_queue_pass1.popleft()
+                detect_queue_pass1.append(formatted_new_point)
+                miss_counter = 0
+        if not keypoints or not formatted_new_point:
+            if miss_counter < 30:
+                miss_counter += 1
+            if miss_counter == 30:
+                if len(detect_queue_pass1):
+                    clear_reason = "idle"
+                detect_queue_pass1.clear()
     except IndexError:
         pass
     frame_count += 1
@@ -380,40 +434,23 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         (255, 255, 255),
         2,
     )
+    cv2.putText(
+        raw_points_image,
+        f"{clear_reason}",
+        (200, 50),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2,
+    )
     cv2.imshow("Raw Points", raw_points_image)
 
     i += 1
     if i == 10:
         if len(detect_queue_pass1) > 0:
-            executor.submit(render, detect_queue_pass1)
-            # render(detect_queue_pass1)
-            # print("[")
-            # for frame in detect_queue_pass1:
-            #     print(f"{frame},")
-            # print("]")
-            # print(f"===== {time.ctime()} ======")
+            # executor.submit(render, detect_queue_pass1)
+            render(detect_queue_pass1)
         i = 0
-
-    # for pt in detect_queue_pass2:
-    #     cv2.circle(
-    #         final_image, (pt[0], pt[1]), radius=3, color=(255, 255, 255), thickness=-1
-    #     )
-    # cv2.imshow("Final Image", final_image)
 
     cv2.waitKey(1)
     rawCapture.truncate(0)
-
-    # i += 1
-    # if i == 30:
-    #     print(
-    #         f"{[(pt[0], pt[1], image[pt[1]][pt[0]]) for pt in detect_queue_pass1]}",
-    #         file=dumpf
-    #     )
-    #     i = 0
-#    pt0 = None
-#    for pt in detect_queue:
-#        if not pt0:
-#            pt0 = pt
-#            continue
-#        cv2.line(image, pt0, pt, 200, 3)
-#        pt0 = pt
