823d1713c53722eadaa49f1b0242e4501328c7ab
Phil D'Amore <ptdamore@gmail.com>
2023-09-09 14:29:13 -0400

    enable websockets

--- esp32/main/main.cpp
+++ esp32/main/main.cpp
@@ -18,7 +18,7 @@
 #include "atan_lut.hpp"
 #include "cos_lut.hpp"
 #include <sys/socket.h>
-#include <netdb.h>            // struct addrinfo
+#include <netdb.h>
 #include "model.hpp"
 
 #include "tensorflow/lite/micro/micro_interpreter.h"
@@ -27,26 +27,6 @@
 #include "tensorflow/lite/schema/schema_generated.h"
 
 using namespace std;
-
-// #define PWDN_GPIO_NUM     32
-// #define RESET_GPIO_NUM    -1
-// #define XCLK_GPIO_NUM      0
-// #define SIOD_GPIO_NUM     26
-// #define SIOC_GPIO_NUM     27
-
-// #define Y9_GPIO_NUM       35
-// #define Y8_GPIO_NUM       34
-// #define Y7_GPIO_NUM       39
-// #define Y6_GPIO_NUM       36
-// #define Y5_GPIO_NUM       21
-// #define Y4_GPIO_NUM       19
-// #define Y3_GPIO_NUM       18
-// #define Y2_GPIO_NUM        5
-// #define VSYNC_GPIO_NUM    25
-// #define HREF_GPIO_NUM     23
-// #define PCLK_GPIO_NUM     22
-
-// #define LED_GPIO_NUM      4
 
 #define PWDN_GPIO_NUM     -1
 #define RESET_GPIO_NUM    40
